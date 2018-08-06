b8d3a088eee9e40a2453f132df0c3ea70fddd994
erkan94 <erk4ntan@hotmail.com>
2018-08-06 21:45:57 +0300

    add stack class

---
+++ src/com/erkan/Stack.java
@@ -0,0 +1,38 @@
+package com.erkan;
+
+public class Stack {
+
+    private char [] myStack;
+    private int top;
+    private int maxSize;
+
+    public Stack(int size) {
+        this.myStack  = new char[size];
+        maxSize = size;
+        top = -1;
+    }
+
+    public void push(char obj){
+        if(!isFull())
+            myStack[++top] = obj;
+        else
+            System.out.println("Stack is full!");
+    }
+
+    public char pop(){
+        if(!isEmpty())
+            return myStack[top--];
+        else{
+            System.out.println("Stack is empty!");
+            return 0;
+        }
+    }
+
+    public boolean isEmpty(){
+        return top==-1;
+    }
+
+    public boolean isFull(){
+        return top==maxSize;
+    }
+}
